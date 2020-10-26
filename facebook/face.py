44683f2d8bda081d710c9bb80f523e3f9dc56182
Diego <dcormace@gmail.com>
2020-10-26 10:04:21 -0300

    First commit

---
+++ facebook/face.py
@@ -0,0 +1,6 @@
+
+with open(filename) as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+content = [x.strip() for x in content]
+
