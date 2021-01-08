c76cc13b8faf2bf3365a06a7dd7f75d2610194b4
Diego <dcormace@gmail.com>
2021-01-08 19:13:33 -0300

    New version of the parser

---
+++ facebook2/parser5.py
@@ -0,0 +1,124 @@
+from bs4 import BeautifulSoup 
+import sys
+
+
+# Reading the data inside the xml 
+# file to a variable under the name 
+# data
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    print("Error: no file name")
+    exit()
+
+with open(filename, 'r') as f: 
+	data = f.read() 
+
+# Passing the stored data inside 
+# the beautifulsoup parser, storing 
+# the returned object 
+Bs_data = BeautifulSoup(data, "xml") 
+
+# Finding all instances of tag 
+b_unique = Bs_data.find_all(['w:hyperlink','w:instrText','w:t']) 
+
+index = 0
+while index < len(b_unique):
+
+    data = b_unique[index]
+
+    if data.name == 'instrText':
+        index = index + 1
+        #if data.string.find("padremariopantaleo") < 0:
+        data = b_unique[index]
+
+        fandestacado = ' '
+        if data.name == 't' and data.string.find('Fan destacado') >= 0:
+            fandestacado = data.string
+            index = index + 1
+            data = b_unique[index]
+
+        if data.name == 'hyperlink':
+            tags = data.find_all('w:t')
+            if len(tags) > 0:
+                #print("Encontre un nuevo comentario!!!!!!!!!!!!!!!!!!!!!!!!")
+                #print("-------------------------------")
+                nombre = ''
+                for tag in tags:
+                    nombre += tag.string
+                    index = index + 1
+
+            comentario = ''
+            index = index + 1
+            data = b_unique[index]
+            while data.name == 't' and data.string.find('Me gusta', 0, 9) < 0:
+                comentario = comentario + data.string
+                index = index + 1
+                data = b_unique[index]
+
+            print("C ; " + nombre.replace(';', ',') + " ; " + fandestacado + " ; " + comentario.replace(';', ','))
+
+    elif data.name == 't':
+        if data.string.find("Obra del Padre Mario", 0, 22) >= 0:
+            #print("Encontre un nuevo POSTEO!!!!!!!!!!!!!!!!!!!!!!!!")
+            #print("-------------------------------")
+            index = index + 3
+            data = b_unique[index]
+            while data.name != 't':
+                index = index + 1
+                if index >= len(b_unique):
+                    exit()
+                data = b_unique[index]
+
+            posteo = ''
+            fecha = ''
+            while data.string.find('comentario', 0, 14) < 0 and data.string.find('Me gusta', 0, 14) < 0:
+                if data.name == 't':
+                    if data.string.find("Fecha", 0, 8) >= 0:
+                        fecha = data.string
+                        while data.string.find(")") < 0:
+                            index += 1
+                            data = b_unique[index]
+                            fecha += data.string
+                        for tmp in ["(","Fecha",":",")"]:
+                            fecha = fecha.replace(tmp,"")
+                        posteo = ''
+                    else:
+                        posteo = posteo + data.string
+
+                index = index + 1
+                if index == len(b_unique):
+                    exit()
+
+                #print(index, len(b_unique))
+                data = b_unique[index]
+                while data.name != 't':
+                    index = index + 1
+                    if index >= len(b_unique):
+                        exit()
+                    data = b_unique[index]
+            
+            comentarios = ""
+            if data.string.find('comentario', 0, 14) > 0:
+                comentarios = data.string
+                index += 1
+                if index >= len(b_unique):
+                    exit()
+                data = b_unique[index]
+
+            compartido = ""
+            if data.string.find("compartid", 0, 25) > 0:
+                compartido = data.string
+                index += 1
+                if index >= len(b_unique):
+                    exit()
+                data = b_unique[index]
+
+            print("P ; " + fecha + " ; " + comentarios + " ; " + compartido + " ; Obra del Padre Mario ; " + posteo.replace(';',','))
+
+
+        index = index + 1
+
+    else:
+        index = index + 1
+
